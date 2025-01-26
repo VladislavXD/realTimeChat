fd0c9aecfe5f109eca661bfae05f7a61d75a75b7
Vladislav <neivo@vladislav.dev>
2025-01-27 00:54:19 +0500

    add script for copy redirect file in build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import copy from 'vite-plugin-static-copy'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  copy({
+    targets: [
+      { src: '_redirects', dest: '' }
+    ]
+  })],
 })
